aae45e1652089b27ce6fb50badcd28369e8ea0ec
asettar <achrafsettar8@gmail.com>
2025-10-31 16:45:10 +0100

    add message validation

--- formValidate.js
+++ formValidate.js
@@ -3,7 +3,8 @@ function checkFeild(event, inputId) {
     if (event.key !== 'Enter' && event.key !== 'Tab') return;
     if (inputId === 'firstname' || inputId == 'secondname') checkName(event, inputId);
     else if (inputId == 'mail') checkMail(event, inputId);
-    else checkPhoneNumber(event, inputId)
+    else if (inputId == "phonenum") checkPhoneNumber(event, inputId);
+    else checkMessage(event, inputId);
 }
 
 
@@ -28,10 +29,12 @@ function    checkName(event, inputId) {
         p.innerHTML = val[1];
         inputElement.style["border-color"] = 'red'; 
         event.preventDefault();
+        return false;
     }
     else {
         p.innerHTML = "";
         inputElement.style["border-color"] = 'green';
+        return true;
     }
 }
 
@@ -54,11 +57,12 @@ function checkMail(event, inputId) {
         p.innerHTML = val[1];
         mailInputElement.style["border-color"] = 'red'; 
         event.preventDefault();
-        
+        return false;
     }
     else {
         p.innerHTML = "";
         mailInputElement.style["border-color"] = 'green';
+        return true;
     }
 }
 
@@ -84,16 +88,48 @@ function checkPhoneNumber(event, inputId) {
         p.innerHTML = val[1];
         phonenumInputElement.style["border-color"] = 'red'; 
         event.preventDefault();
+        return false;
     }
     else {
-        p.innerHTML = "";
         phonenumInputElement.style["border-color"] = 'green';
+        return true;
     }
 }
+
+
+function checkMessage(event, inputId) {
+    console.log(event.key);
+    const messageElement = document.getElementById(inputId); 
+    const p = document.querySelector('.msg-errormsg');
+    
+    if (messageElement.value.length > 0) {
+        console.log("value found:", messageElement.value, messageElement.value.length);
+        messageElement.style.setProperty('border-color', 'green');
+        p.innerHTML = "";
+        // messageElement.style["border-color"] = 'green'; 
+        return true;
+    }
+    else {
+        messageElement.style["border-color"] = 'gray';
+        p.innerHTML = "the message is required";
+        return false;
+    }
+}
+
+// function   validateForm(event) { 
+//     console.log("submit clicked");
+//     event.preventDefault();
+//     if (!checkName(event, "firstname") || !checkName(event, "secondname") 
+// 	   || !checkMail(event, "mail")  || !checkPhoneNumber(event, "phonenum")
+// 	   || !checkMessage()
+// 	)
+//         return;
+    // const success = document.querySelector('.success-msg');        
+    // success.style.display = 'block';
+// }
 
 function    isFormValid() {
     checkFirstName();
 }
 
 console.log("Hello world");
-// isFormValid();
