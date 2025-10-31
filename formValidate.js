9076e98b6a45871d2276f9bd0661ed73f0f79937
asettar <achrafsettar8@gmail.com>
2025-10-31 12:08:24 +0100

    add mail validation

--- formValidate.js
+++ formValidate.js
@@ -2,12 +2,13 @@ function checkFeild(event, inputId) {
     console.log(event.key);
     if (event.key !== 'Enter' && event.key !== 'Tab') return;
     if (inputId === 'firstname' || inputId == 'secondname') checkName(event, inputId);
-    else if (inputId == 'mail') checkMail(event);
+    else if (inputId == 'mail') checkMail(event, inputId);
 }
 
 
 function    isValidName(name) {
     if (!name) return [false, "this feild is required."];
+    if (name.length < 4) return[false, "name length should be >= 4"]
     for (let c of name) {
         // console.debug(c.toLowerCase(), c.toUpperCase());
         if (c.toUpperCase() !== c.toLowerCase() == false) return [false, "name should contain only alphabectic characters"];
@@ -34,24 +35,31 @@ function    checkName(event, inputId) {
 }
 
 
-// function checkMail() {
-//     const mailInputElement = document.getElementById(inputId); 
-//     console.log(mailInputElement);
-//     const p = document.querySelector('.mail-errormsg');
-//     let val = isValidMail(inputElement.value);
-//     if (val[0] == false) {
-//         console.log("Mail Invalid::");
-//         console.log(p, val[1]);
-//         p.innerHTML = val[1];
-//         inputElement.style["border-color"] = 'red'; 
-//         event.preventDefault();
+function isValidMail(mail) {
+    if (mail.length <= 8) return [false, "mail lenght should be > 8,"]
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = pattern.test(mail);
+    if (isValid) return [true];
+    else return [false, "invalid mail"];
+}
+
+function checkMail(event, inputId) {
+    const mailInputElement = document.getElementById(inputId); 
+    console.log(mailInputElement);
+    const p = document.querySelector('.mail-errormsg');
+    let val = isValidMail(mailInputElement.value);
+    if (val[0] == false) {
+        console.log("Mail Invalid::");
+        p.innerHTML = val[1];
+        mailInputElement.style["border-color"] = 'red'; 
+        event.preventDefault();
         
-//     }
-//     else {
-//         p.innerHTML = "";
-//         inputElement.style["border-color"] = 'green';
-//     }
-// }
+    }
+    else {
+        p.innerHTML = "";
+        mailInputElement.style["border-color"] = 'green';
+    }
+}
 
 function    isFormValid() {
     checkFirstName();
